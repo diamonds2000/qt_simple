94e39193a27fb54004a359bbf50985d388cf850e
diamonds2000 <diamonds2000@163.com>
2026-06-02 01:01:21 +0800

    Fix UI issues

--- MainWindow.h
+++ MainWindow.h
@@ -1,6 +1,6 @@
 #include <QMainWindow>
 
-class QTextEdit;
+class QLineEdit;
 class QPushButton;
 class QLabel;
 class QVBoxLayout;
@@ -21,14 +21,14 @@ private:
     QVBoxLayout* m_layout = nullptr;
     QGridLayout* m_subLayout = nullptr;
 
-    QTextEdit* m_inputVideo = nullptr;
-    QTextEdit* m_outputVideo = nullptr;
-    QTextEdit* m_logOutput = nullptr;
+    QLineEdit* m_inputVideo = nullptr;
+    QLineEdit* m_outputVideo = nullptr;
+    QLineEdit* m_logOutput = nullptr;
 
-    QTextEdit* m_packedInfo = nullptr;
-    QTextEdit* m_videoFormat = nullptr;
-    QTextEdit* m_videoCodec = nullptr;
-    QTextEdit* m_videoBitrate = nullptr;
-    QTextEdit* m_videoResolution = nullptr;
-    QTextEdit* m_audioCodec = nullptr;
+    QLineEdit* m_packedInfo = nullptr;
+    QLineEdit* m_videoFormat = nullptr;
+    QLineEdit* m_videoCodec = nullptr;
+    QLineEdit* m_videoBitrate = nullptr;
+    QLineEdit* m_videoResolution = nullptr;
+    QLineEdit* m_audioCodec = nullptr;
 };
